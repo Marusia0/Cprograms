d68577d4d79c46d62b81462b5b79e588c5f870d1
Marusia0 <lunapelota@hotmail.com>
2017-08-07 23:05:45 -0700

    Add files via upload

---
+++ Lab01/first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//1 write a 
+int main(){
+
+	int a = 4;
+	int b = 3;
+	int sum = 0;
+
+	sum = a + b;
+
+	printf("%d + %d = %d\n", a, b, sum);
+	printf("The ");
+	return 0;
+
+}
---
+++ Lab01/lab1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	printf("Binary of 12 is: 1100\n");
+	printf("Binary of 185 is:10111001\n");
+	printf("Binary of 294 is: 10010110\n");
+
+	printf("Decimal of 101101 is: 45\n");
+	printf("Decimal of 11101011 is: 235\n");
+	printf("Decimal of 1010 is: 10\n");
+
+	printf("The Decimal of 1A is 11\n");
+	printf("The Decimal of 3F31 is 22\n");
+	printf("The Decimal of 31C is 16\n");
+
+	printf("The Hexadecimal of 10010111 is: 97\n");
+	printf("The Hexadecimal of 101101111110 is: B7E\n");
+	printf("The Hexadecimal of 1111 is: F\n");
+
+	printf("The Binary of 1A3B is: 0001101000111011\n");
+	printf("The Binary of ABCD is: 1010101111001101\n");
+	printf("The Binary of 111111 is: 000100010001000100010001\n");
+	
+	return 0;
+}
+
+
+
